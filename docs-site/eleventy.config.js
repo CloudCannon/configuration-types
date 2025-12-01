8b86f5020dfa5e61ce9adf8021eb0ba67e1c2720
Ross Phillips <12723297+rphillips-cc@users.noreply.github.com>
2025-12-02 11:33:06 +1300

    Add nav to example site

--- docs-site/eleventy.config.js
+++ docs-site/eleventy.config.js
@@ -3,9 +3,27 @@ import { RenderPlugin } from "@11ty/eleventy";
 
 export default function (eleventyConfig) {
 	eleventyConfig.addPlugin(RenderPlugin);
-	eleventyConfig.addGlobalData('documentationPages', Object.values(documentation));
+	eleventyConfig.addGlobalData('docs', Object.values(documentation));
 
-	eleventyConfig.addFilter("json", (json) => JSON.stringify(json, undefined, '  '));
+	eleventyConfig.addFilter("json", (json) => {
+		if (typeof json === 'object') {
+			return JSON.stringify({ ...json, page: undefined, eleventy: undefined }, undefined, '  ');
+		}
+
+		return JSON.stringify(json, undefined, '  ');
+	});
+	eleventyConfig.addFilter("isGidInside", (gid, parentGid) => parentGid === 'type.Configuration' ?
+		!gid.startsWith('type.') : gid.startsWith(parentGid + '.')
+	);
+	eleventyConfig.addFilter("parentGidsFromDoc", (doc) => {
+		const parentGids = [];
+		let parentGid = doc.parent;
+		while (parentGid) {
+			parentGids.unshift(parentGid);
+			parentGid = documentation[parentGid].parent;
+		}
+		return parentGids;
+	});
 	eleventyConfig.addFilter("docFromGid", (gid) => documentation[gid]);
 	eleventyConfig.addFilter("docFromRef", (docRef) => {
 		const doc = documentation[docRef?.gid] || docRef;
