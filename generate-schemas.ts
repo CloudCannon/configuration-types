b8ca5eb6222f2cb51c0d6830db345ffa17cda77d
Ross Phillips <12723297+rphillips-cc@users.noreply.github.com>
2025-12-03 12:51:10 +1300

    feat: new schemas for split configuration

--- generate-schemas.ts
+++ generate-schemas.ts
@@ -5,12 +5,16 @@ import { JekyllConfigurationSchema } from './src/build-coupled';
 import { HugoConfigurationSchema } from './src/build-coupled';
 import { EleventyConfigurationSchema } from './src/build-coupled';
 import { ReaderConfigurationSchema } from './src/build-coupled';
+import { CollectionConfigSchema } from './src/collections';
 import { ConfigurationSchema } from './src/configuration';
+import { EditablesSchema } from './src/editables';
+import { InputsSchema } from './src/inputs';
+import { SnippetsImportsSchema } from './src/snippets';
+import { StructureValueSchema, StructuresSchema } from './src/structures';
 
 const schemas = [
 	{
 		schema: ConfigurationSchema.meta({
-			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-config.latest.schema.json',
 			id: 'type.Configuration',
 		}),
 		keepDocumentationType: true,
@@ -21,7 +25,6 @@ const schemas = [
 		schema: ConfigurationSchema.meta({
 			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-config.latest.schema.json',
 		}),
-		target: 'draft-7' as const,
 		filename: 'cloudcannon-config.latest.schema.json',
 		convertSchemaAnys: true,
 		addMarkdownDescription: true,
@@ -31,7 +34,6 @@ const schemas = [
 		schema: JekyllConfigurationSchema.meta({
 			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-config.legacy-jekyll.schema.json',
 		}),
-		target: 'draft-7' as const,
 		filename: 'cloudcannon-config.legacy-jekyll.schema.json',
 		convertSchemaAnys: true,
 		addMarkdownDescription: true,
@@ -41,7 +43,6 @@ const schemas = [
 		schema: HugoConfigurationSchema.meta({
 			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-config.legacy-hugo.schema.json',
 		}),
-		target: 'draft-7' as const,
 		filename: 'cloudcannon-config.legacy-hugo.schema.json',
 		convertSchemaAnys: true,
 		addMarkdownDescription: true,
@@ -51,7 +52,6 @@ const schemas = [
 		schema: EleventyConfigurationSchema.meta({
 			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-config.legacy-eleventy.schema.json',
 		}),
-		target: 'draft-7' as const,
 		filename: 'cloudcannon-config.legacy-eleventy.schema.json',
 		convertSchemaAnys: true,
 		addMarkdownDescription: true,
@@ -61,8 +61,106 @@ const schemas = [
 		schema: ReaderConfigurationSchema.meta({
 			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-config.legacy-reader.schema.json',
 		}),
-		target: 'draft-7' as const,
 		filename: 'cloudcannon-config.legacy-reader.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: _structures_from_glob
+		// files: *.cloudcannon.structures.(yml|yaml|json)
+		schema: StructuresSchema.meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-structures.schema.json',
+		}),
+		filename: 'cloudcannon-structures.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: _inputs_from_glob
+		// files: *.cloudcannon.inputs.(yml|yaml|json)
+		schema: InputsSchema.meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-inputs.schema.json',
+		}),
+		filename: 'cloudcannon-inputs.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: values_from_glob
+		// files: *.cloudcannon.structure-value.(yml|yaml|json)
+		schema: StructureValueSchema.meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-structure-value.schema.json',
+		}),
+		filename: 'cloudcannon-structure-value.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: _snippets_from_glob, _snippets_templates_from_glob
+		// files: *.cloudcannon.snippets.(yml|yaml|json)
+		schema: ConfigurationSchema.shape._snippets.unwrap().meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-snippets.schema.json',
+		}),
+		filename: 'cloudcannon-snippets.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: _snippets_imports_from_glob
+		// files: *.cloudcannon.snippets-imports.(yml|yaml|json)
+		schema: SnippetsImportsSchema.meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-snippets-imports.schema.json',
+		}),
+		filename: 'cloudcannon-snippets-imports.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: _snippets_definitions_from_glob
+		// files: *.cloudcannon.snippets-definitions.(yml|yaml|json)
+		schema: ConfigurationSchema.shape._snippets_definitions.unwrap().meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-snippets-definitions.schema.json',
+		}),
+		filename: 'cloudcannon-snippets-definitions.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: _editables_from_glob
+		// files: *.cloudcannon.editables.(yml|yaml|json)
+		schema: EditablesSchema.meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-editables.schema.json',
+		}),
+		filename: 'cloudcannon-editables.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: collections_config_from_glob
+		// files: *.cloudcannon.collections.(yml|yaml|json)
+		schema: ConfigurationSchema.shape.collections_config.unwrap().meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-collections.schema.json',
+		}),
+		filename: 'cloudcannon-collections.schema.json',
+		convertSchemaAnys: true,
+		addMarkdownDescription: true,
+		stripId: true,
+	},
+	{
+		// keys: schemas_from_glob
+		// files: *.cloudcannon.schemas.(yml|yaml|json)
+		schema: CollectionConfigSchema.shape.schemas.unwrap().meta({
+			$id: 'https://github.com/cloudcannon/configuration-types/releases/latest/download/cloudcannon-schemas.schema.json',
+		}),
+		filename: 'cloudcannon-schemas.schema.json',
 		convertSchemaAnys: true,
 		addMarkdownDescription: true,
 		stripId: true,
@@ -71,7 +169,7 @@ const schemas = [
 
 for (const schema of schemas) {
 	const jsonSchema = z.toJSONSchema(schema.schema, {
-		target: schema.target,
+		target: schema.target || 'draft-7',
 		override: (ctx) => {
 			if (
 				schema.convertSchemaAnys &&
